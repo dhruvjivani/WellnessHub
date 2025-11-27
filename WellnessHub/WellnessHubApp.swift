212bd8bc82d8cb8f35de3bd5ae48a52cf1fa0752
dhruvjivani <dhruvjivani001@gmail.com>
2025-11-26 19:11:21 -0500

    Initial Commit

---
+++ WellnessHub/WellnessHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessHubApp.swift
+//  WellnessHub
+//
+//  Created by Dhruv Rasikbhai Jivani on 11/26/25.
+//
+
+import SwiftUI
+
+@main
+struct WellnessHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
